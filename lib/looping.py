0e8e59ced501a23ea8a40f275df52a3e4e5e8bd6
livotieno <livio5261@gmail.com>
2023-11-22 19:18:09 +0300

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -3,11 +3,32 @@
 def happy_new_year():
     # code goes here!
     pass
+      i=10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+        i-=1 
 
 def square_integers(int_list):
     # code goes here!
     pass
+    squared_integers = [integer ** 2 for integer in int_list]
+    return squared_integers
 
 def fizzbuzz():
     # code goes here!
     pass
+    num = 1
+   while num <= 100:
+        if num % 3 == 0 and num % 5 == 0  :
+           print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+        
+
